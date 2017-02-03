72c0a8c72dd623854f80ffdce3512e945cca4699
M <michael.halligan@hotschedules.com>
2017-02-03 16:12:56 +0300

    INFRA-4294: Remove dollars from keys in default config hashes

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,9 +10,9 @@
 
 class puppet (
 
-  $agent_hash                 = getvar("::puppet::params::puppetconf::default_agent_hash"),
-  $master_hash                = getvar("::puppet::params::puppetconf::master_agent_hash"),
-  $main_hash                  = getvar("::puppet::params::puppetconf::main_agent_hash")
+  $agent_hash                 = getvar("::puppet::params::default_agent_hash"),
+  $master_hash                = getvar("::puppet::params::master_agent_hash"),
+  $main_hash                  = getvar("::puppet::params::main_agent_hash")
 
 ) inherits puppet::params {
   
--- manifests/params/puppetconf.pp
+++ manifests/params/puppetconf.pp
@@ -11,34 +11,34 @@ class puppet::params::puppetconf {
   $configfile = '/etc/puppet/puppet.conf'
 
   $default_main_hash = {
-    $disable_warnings           => 'deprecations',
-    $logdir                     => '/var/log/puppet',
-    $rundir                     => '/var/run/puppet',
-    $ssldir                     => '$vardir/ssl',
+    disable_warnings           => 'deprecations',
+    logdir                     => '/var/log/puppet',
+    rundir                     => '/var/run/puppet',
+    ssldir                     => '$vardir/ssl',
   }
 
   $default_agent_hash = {
-    $autoflush 				          => true,
-    $certname 				          => $::fqdn,
-    $configtimeout 		          => 3600,
-    $disable_warnings           => 'deprecations',
-    $environment 			          => $::environment,
-    $listen 					          => false,
-    $pluginsync 			          => true,
-    $server 					          => $::servername,
-    $splay 					            => true,
+    autoflush 				         => true,
+    certname 				           => $::fqdn,
+    configtimeout 		         => 3600,
+    disable_warnings           => 'deprecations',
+    environment 			         => $::environment,
+    listen 					           => false,
+    pluginsync 			           => true,
+    server 					           => $::servername,
+    splay 					           => true,
   }
 
   $default_master_hash = {
-    $autosign                   => true,
-    $certname                   => $::servername,
-    $dns_alt_names              => 'puppet,dev-puppet-01',
-    $environment                => $::environment,
-    $filetimeout                => 1,
-    $manifest                   => '$confdir/environments/$environment/manifests',
-    $modulepath                 => '$confdir/environments/$environment/modules:$confdir/environments/$environment/dist',
-    $pluginsync                 => true,
-    $ssl_client_header          => 'SSL_CLIENT_S_DN',
-    $ssl_client_verify_header	  => 'SSL_CLIENT_VERIFY', 
+    autosign                   => true,
+    certname                   => $::servername,
+    dns_alt_names              => 'puppet,dev-puppet-01',
+    environment                => $::environment,
+    filetimeout                => 1,
+    manifest                   => '$confdir/environments/$environment/manifests',
+    modulepath                 => '$confdir/environments/$environment/modules:$confdir/environments/$environment/dist',
+    pluginsync                 => true,
+    ssl_client_header          => 'SSL_CLIENT_S_DN',
+    ssl_client_verify_header	 => 'SSL_CLIENT_VERIFY', 
   }
 } 
