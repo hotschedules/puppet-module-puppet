b20ed1719c80b588d0d9a176110386b296b6f44c
M <michael.halligan@hotschedules.com>
2017-02-15 17:45:29 +0300

    INFRA-4294: Add some checking around role for master config, probably doing this wrong

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,36 +9,49 @@
 #
 class puppet (
 
-  $agent_hash     = getvar("::puppet::params::default::agent"),
-  $hieramerge     = false,
-  $main_hash      = getvar("::puppet::params::default::main"),
-  $master_hash    = getvar("::puppet::params::default::master"),
-  $pkg            = getvar("::puppet::params::default::pkg"),
-  $version        = getvar("::puppet::params::default::version"),
-  $svcensure      = getvar("::puppet::params::default::svcensure"),
-  $svcenable      = getvar("::puppet::params::default::svcenable"),
-  $svcname        = getvar("::puppet::params::default::svcname"),
+  $agent_hash      = getvar("::puppet::params::default::agent"),
+  $agentsvcenable  = getvar("::puppet::params::default::agentsvcenable"),
+  $agentsvcensure  = getvar("::puppet::params::default::agentsvcensure"),
+  $agentsvcname    = getvar("::puppet::params::default::agentsvcname"),
+  $hieramerge      = getvar("::puppet::params::default::hieramerge"),,
+  $main_hash       = getvar("::puppet::params::default::main"),
+  $master_hash     = getvar("::puppet::params::default::master"),
+  $mastersvcenable = getvar("::puppet::params::default::mastersvcenable"),
+  $mastersvcensure = getvar("::puppet::params::default::mastersvcensure"),
+  $mastersvcname   = getvar("::puppet::params::default::mastersvcname"),
+  $pkg             = getvar("::puppet::params::default::masterpkg"),
+  $version         = getvar("::puppet::params::default::version"),
 
 ) inherits puppet::params {
 
   validate_bool             ( $hieramerge     )
-  validate_string           ( $pkg            )
+  validate_string           ( $agentpkg            )
   validate_string           ( $version        )
-  validate_bool             ( $svcenable      )
-  validate_string           ( $svcname        )
-  validate_bool             ( $svcensure      )
+  validate_bool             ( $agentsvcenable      )
+  validate_string           ( $agentsvcname        )
+  validate_bool             ( $agentsvcensure      )
+
+  if %{::instance_role} == 'puppet' {
+    validate_string           ( $masterpkg            )
+    validate_bool             ( $mastersvcenable      )
+    validate_string           ( $mastersvcname        )
+    validate_bool             ( $mastersvcensure      )
+  }
   
   # Merge config hashes
   if $hieramerge {
     $x_agent  = hiera_hash('puppet::agent',  $agent_hash)
     $x_main   = hiera_hash('puppet::main',   $main_hash)
-    $x_master = hiera_hash('puppet::master', $master_hash)
+    if %{::instance_role} == 'puppet' {
+      $x_master = hiera_hash('puppet::master', $master_hash)
+    }
   } else {
     $x_agent  = $agent_hash
     $x_main   = $main_hash
-    $x_master = $master_hash
+    if %{::instance_role} == 'puppet' {
+      $x_master = $master_hash
+    }
   }
-
   
   # [agent] section parameters
   validate_bool 				  ( $x_agent["autoflush"]                  )
@@ -57,17 +70,19 @@ class puppet (
   validate_absolute_path  ( $x_main["rundir"]                      )
   validate_string         ( $x_main["ssldir"]                      )
   
-  # [master] section parameters
-  validate_bool           ( $x_master["autosign"]                  )
-  validate_string         ( $x_master["certname"]                  )
-  validate_string         ( $x_master["dns_alt_names"]             )
-  validate_string         ( $x_master["environment"]               )
-  validate_integer        ( $x_master["filetimeout"]               )
-  validate_string         ( $x_master["manifest"]                  )
-  validate_string         ( $x_master["modulepath"]                )
-  validate_bool           ( $x_master["pluginsync"]                )
-  validate_string         ( $x_master["ssl_client_header"]         )
-  validate_string         ( $x_master["ssl_client_verify_header"]  )
+  if %{::instance_role} == 'puppet' {
+    # [master] section parameters
+    validate_bool           ( $x_master["autosign"]                  )
+    validate_string         ( $x_master["certname"]                  )
+    validate_string         ( $x_master["dns_alt_names"]             )
+    validate_string         ( $x_master["environment"]               )
+    validate_integer        ( $x_master["filetimeout"]               )
+    validate_string         ( $x_master["manifest"]                  )
+    validate_string         ( $x_master["modulepath"]                )
+    validate_bool           ( $x_master["pluginsync"]                )
+    validate_string         ( $x_master["ssl_client_header"]         )
+    validate_string         ( $x_master["ssl_client_verify_header"]  )
+  }
 
   $x_conf_hash = {
     agentconf => {
